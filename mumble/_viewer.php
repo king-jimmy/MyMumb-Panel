95e108541e2c6af209a23bf10766a4d0c20696be
Damien Calesse <madeinjb@gmail.com>
2015-02-09 15:18:54 +0100

    Add Simple Mumble Viewer
    Add a simple Mumble Viewer.
    Display the server's name, channels and users

--- mumble/_viewer.php
+++ mumble/_viewer.php
@@ -1,3 +1,36 @@
 <center>
 <br><br>
-<span style="font-size:22px;"><?php echo $LANGUAGE['viewer_notyet'];?></span>
+<span style="font-size:22px;"> Mumble viewer </span>
+<br><br>
+</center>
+<div class="well">
+<?php
+
+  if (isset($Server)) {
+
+    function displayChannels($tree, $indent)
+    {
+
+      for ($i=0; $i<$indent; $i++)
+        echo '<span class="indent-channel"> </span>';
+      echo ($indent == 0) ? '<p>' : '';
+        echo '<span id="'. $tree->c->id .'">'. $tree->c->name .'</span><br>';
+        foreach($tree->users as $user) {
+          for ($i=0; $i<$indent; $i++)
+            echo '<span class="indent-channel"> </span>';
+          echo '<span class="indent-user"> </span>';
+          echo '<span id="'. $user->userid .'">'. $user->name .'</span>';
+        }
+        echo (!empty($tree->users)) ? '<br>' : '';
+        foreach($tree->children as $channel)
+          displayChannels($channel, $indent+1);
+      echo ($indent == 0) ? '</p>' : '';
+    }
+
+    // Display server name
+    echo '<center><p>'.$Server->getConf('registername').'</p></center>';
+
+    // Display channels & users
+    displayChannels($Server->getTree(), 0);
+  }
+echo '</div>';
